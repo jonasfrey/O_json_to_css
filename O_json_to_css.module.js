003fad47354d0030ae55de1b394839d50bf8e558
jf18j492 <jonas.frey@csh.unibe.ch>
2022-09-28 01:11:52 +0200

    fixed broken 0.2 version

--- O_json_to_css.module.js
+++ O_json_to_css.module.js
@@ -1,3 +1,5 @@
+import {f_evaluate_object} from "https://deno.land/x/f_evaluate_object@0.1/f_evaluate_object.module.js"
+
 
 class O_json_to_css{
     constructor(
@@ -54,3 +56,149 @@ class O_json_to_css{
     }
 
 }
+export {O_json_to_css}
+
+// usage
+// var o = {
+//     o_functions: {
+//         f_s_color_to_rgba(o_color){
+//             return `rgba(${o_color.r},${o_color.g},${o_color.b},${o_color.alpha})`
+//         }
+//     },
+//     o_themes: {
+//         o_dark: {
+//             o_colors: {
+//                 "o_background": {
+//                     r: 22, 
+//                     g: 22, 
+//                     b: 22, 
+//                     alpha: 0.9,
+//                 },
+//                 "o_foreground": {
+//                     r: 222, 
+//                     g: 222, 
+//                     b: 222, 
+//                     alpha: 0.9,
+//                 },
+//                 "o_alpha": {
+//                     r: 0, 
+//                     g: 0, 
+//                     b: 0, 
+//                     alpha: 0.9,
+//                 },
+//                 "o_beta": {
+//                     r: 0, 
+//                     g: 0, 
+//                     b: 0, 
+//                     alpha: 0.9,
+//                 },
+//                 "o_gamma": {
+//                     r: 0, 
+//                     g: 0, 
+//                     b: 0, 
+//                     alpha: 0.9,
+//                 },
+//                 "o_red": {
+//                     r: 0, 
+//                     g: 0, 
+//                     b: 0, 
+//                     alpha: 0.9,
+//                 },
+//                 "o_green": {
+//                     r: 0, 
+//                     g: 0, 
+//                     b: 0, 
+//                     alpha: 0.9,
+//                 },
+//                 "o_blue": {
+//                     r: 0, 
+//                     g: 0, 
+//                     b: 0, 
+//                     alpha: 0.9,
+//                 },
+//             }
+//         }, 
+//         o_light: {
+//             o_colors: {
+//                 "o_background": {
+//                     r: 222, 
+//                     g: 222, 
+//                     b: 222, 
+//                     alpha: 0.9,
+//                 },
+//                 "o_foreground": {
+//                     r: 22, 
+//                     g: 22, 
+//                     b: 22, 
+//                     alpha: 0.9,
+//                 },
+//                 "o_alpha": {
+//                     r: 0, 
+//                     g: 0, 
+//                     b: 0, 
+//                     alpha: 0.9,
+//                 },
+//                 "o_beta": {
+//                     r: 0, 
+//                     g: 0, 
+//                     b: 0, 
+//                     alpha: 0.9,
+//                 },
+//                 "o_gamma": {
+//                     r: 0, 
+//                     g: 0, 
+//                     b: 0, 
+//                     alpha: 0.9,
+//                 },
+//                 "o_red": {
+//                     r: 0, 
+//                     g: 0, 
+//                     b: 0, 
+//                     alpha: 0.9,
+//                 },
+//                 "o_green": {
+//                     r: 0, 
+//                     g: 0, 
+//                     b: 0, 
+//                     alpha: 0.9,
+//                 },
+//                 "o_blue": {
+//                     r: 0, 
+//                     g: 0, 
+//                     b: 0, 
+//                     alpha: 0.9,
+//                 },
+//             }
+//         }
+//     },
+//     o_css: {
+//         o_ruleset: 
+//         {
+//             "s_selector": " body", 
+//             o_rules:{
+//                     "font-size": "1.2rem"
+//                 },
+//             a_o_ruleset: [
+//                 {
+//                     "s_selector": " .theme_dark, .theme_dark2", 
+//                     o_rules:{
+//                             "background-color": "${o_functions.f_s_color_to_rgba(o_themes.o_dark.o_colors.o_background)}",
+//                             "color": "${o_functions.f_s_color_to_rgba(o_themes.o_dark.o_colors.o_foreground)}"
+//                         },
+//                 },
+//                 {
+//                     "s_selector": " .theme_light", 
+//                     o_rules:{
+//                             "background-color": "${o_functions.f_s_color_to_rgba(o_themes.o_light.o_colors.o_background)}",
+//                             "color": "${o_functions.f_s_color_to_rgba(o_themes.o_light.o_colors.o_foreground)}"
+//                         },
+//                 }
+//             ]
+//         }
+        
+//     }
+// }
+
+// var o_json_to_css = new O_json_to_css(o); 
+
+// await o_json_to_css.f_render();
